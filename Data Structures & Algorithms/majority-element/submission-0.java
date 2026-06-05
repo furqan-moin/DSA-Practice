7eeb4fc26774224e63623ab75f36cb0bb1002878
Furqan Moin <furqan.moin7@gmail.com>
2026-06-05 18:03:16 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       Arrays.sort(nums);
+        int count = 0; 
+        if(nums.length==1) return nums[0];
+        for(int i =0 ; i<nums.length-1; i++){
+            if(nums[i]!=nums[i+1])
+            count = 0;
+            else
+            count++;
+            if(count>=(nums.length/2))
+            return nums[i];
+        }
+        return 0;
+        }
+    }
